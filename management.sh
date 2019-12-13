8577eb95547554df546e562469ec20927dd2dda6
Yao-Chung Chen <40789913+ferygood@users.noreply.github.com>
2019-12-13 12:17:57 +0800

    add ugoa

--- management.sh
+++ management.sh
@@ -5,3 +5,6 @@ chown user_name file_name
 
 #option -R is for all file
 chmod -R 744 file_name
+
+# chmod [ugoa], u=file owner, g=group accounts, o=others, a=u+g+o
+chmod a-x file_name # delete executable function for all user
